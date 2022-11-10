31a030bb20942226bec8ba7eb88d25a04b155937
jkronen04 <jacobkronen@gmail.com>
2022-11-10 11:28:50 -0500

    start file

--- main.py
+++ main.py
@@ -16,5 +16,19 @@ if response:
 elif response.status_code == 404:
     print("Not found.")
 
-    print(response.content)
-    response.headers['viewpoint']
+soup = BeautifulSoup(page.content,"html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+print(results.prettify())
+
+job_elements = results.find_all("div",class_="card-content")
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element)
+    print(company_element)
+    print(location_element)
+    print()

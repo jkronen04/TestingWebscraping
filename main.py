44f00cf3d5d1bdf0b54f45531e8f1fe09cd2d74b
jkronen04 <jacobkronen@gmail.com>
2022-11-10 11:09:15 -0500

    start file

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+# requests.get('https://api.github.com')
+
+print(page.text)
+
+response = requests.get('https://realpython.github.io/fake-jobs/')
+
+if response:
+    print("Success!")
+    print(response.status_code)
+elif response.status_code == 404:
+    print("Not found.")
+
+    print(response.content)
+    response.headers['viewpoint']
